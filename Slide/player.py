939d1f2217855c2878cf477d7f02e218183a156a
gaon <asd123g56@gmail.com>
2025-11-05 15:57:18 +0900

    플레이어 클래스 뼈대

---
+++ Slide/player.py
@@ -0,0 +1,11 @@
+from pico2d import *
+
+class Player:
+    def __init__(self):
+        pass
+    def update(self):
+        pass
+    def draw(self):
+        pass
+    def handle_event(self, event):
+        pass
