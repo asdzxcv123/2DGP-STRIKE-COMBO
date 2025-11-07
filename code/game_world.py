785497803993fdc4acb80d47ebbc53c0c704693b
gaon <asd123g56@gmail.com>
2025-11-07 23:50:28 +0900

    게임 월드 파일 생성 및 뼈대 구축

---
+++ code/game_world.py
@@ -0,0 +1,20 @@
+world=[]
+
+def add_object():
+    pass
+
+def add_objects():
+    pass
+
+def update():
+    pass
+
+def render():
+
+    pass
+
+def remove_object():
+    pass
+
+def clear():
+    pass
