623c4209d5065898d57444de629b546a265884ef
gaon <asd123g56@gmail.com>
2025-11-07 23:25:48 +0900

    게임 프레임워크 구축

--- code/game_framework.py
+++ code/game_framework.py
@@ -5,16 +5,30 @@ running = None
 stack = None
 
 def change_mode(mode):
+    global stack
+    if(len(stack)>0):
+        stack[-1].finish()
+        stack.pop()
+    stack.append(mode)
+    mode.init()
 
-    pass
 
 def push_mode(mode):
-
+    global stack
+    if(len(stack)>0):
+        stack[-1].pause()
+    stack.append(mode)
+    mode.init()
     pass
 
 def pop_mode():
+    global stack
+    if(len(stack)>0):
+        stack[-1].finish()
+        stack.pop()
+    if(len(stack)>0):
+        stack[-1].resume()
 
-    pass
 
 def quit():
 
@@ -22,5 +36,23 @@ def quit():
     running = False
 
 def run(start_mode):
+    global running, stack
+    running = True
+    stack = [start_mode]
+    start_mode.init()
 
-    pass
+    global frame_time
+    frame_time = 0.0
+    current_time = time.time()
+    while running:
+        stack[-1].handle_events()
+        stack[-1].update()
+        stack[-1].draw()
+
+        frame_time = time.time() - current_time
+        current_time += frame_time
+        frame_rate = 1.0 / frame_time
+
+while (len(stack) > 0):
+    stack[-1].finish()
+    stack.pop()
