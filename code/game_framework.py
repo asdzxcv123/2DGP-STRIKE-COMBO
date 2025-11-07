8c2e00d1782dc997144bcf26648fbbca608fa7fa
gaon <asd123g56@gmail.com>
2025-11-07 23:16:43 +0900

    게임 프렘워크 파일 생성 및 뼈대 구축

---
+++ code/game_framework.py
@@ -0,0 +1,26 @@
+import time
+frame_time = 0.0
+
+running = None
+stack = None
+
+def change_mode(mode):
+
+    pass
+
+def push_mode(mode):
+
+    pass
+
+def pop_mode():
+
+    pass
+
+def quit():
+
+    global running
+    running = False
+
+def run(start_mode):
+
+    pass
