4b8615991279bc6ccc4cf5bc6a507a73a02a808f
gaon <asd123g56@gmail.com>
2025-11-14 12:18:24 +0900

    모션 관련 템플릿

---
+++ code/motion_templet.py
@@ -0,0 +1,20 @@
+class Name:
+    def __init__(self, Player):
+        self.player=Player
+
+        pass
+
+    def enter(self, e):
+
+        pass
+
+    def exit(self):
+
+        pass
+
+    def do(self):
+
+        pass
+    def draw(self,camera):
+
+        pass
